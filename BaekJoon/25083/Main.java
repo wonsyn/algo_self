26e8faaf42f3eca29884bf173fbc37c0bcd5cdbb
wonsyn <sde3715@naver.com>
2022-07-13 23:14:06 +0900

    20220713 bj_25083

---
+++ BaekJoon/25083/Main.java
@@ -0,0 +1,11 @@
+package bj_25083_새싹;
+
+public class Main {
+	public static void main(String[] args) {
+		StringBuilder sb = new StringBuilder();
+		
+		sb.append("         ,r'\"7\nr`-_   ,'  ,/\n \\. \". L_r'\n   `~\\/\n      |\n      |");
+		
+		System.out.println(sb);
+	}
+}
