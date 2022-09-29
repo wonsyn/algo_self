59e694bba0ba16e34f9c2840b82c7b4c2f7d3cea
wonsyn <sde3715@naver.com>
2022-09-29 22:18:21 +0900

    20220929 bj_2407

---
+++ BaekJoon/2407/Main.java
@@ -0,0 +1,26 @@
+package bj_2407_조합;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.math.BigInteger;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int n = Integer.parseInt(st.nextToken());
+        int m = Integer.parseInt(st.nextToken());
+
+        BigInteger sum = BigInteger.ONE;
+        BigInteger div = BigInteger.ONE;
+
+        for(int i = 0; i<m; i++) {
+            sum = sum.multiply(new BigInteger(String.valueOf(n-i)));
+            div = div.multiply(new BigInteger(String.valueOf(i+1)));
+        }
+
+        System.out.println(sum.divide(div));
+    }
+}
