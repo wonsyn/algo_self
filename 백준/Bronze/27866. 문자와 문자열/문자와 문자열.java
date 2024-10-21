2988196f9af34e23edaad22f72a6623ba688e607
wonsyn <82020255+wonsyn@users.noreply.github.com>
2024-10-21 16:34:39 +0900

    [Bronze V] Title: 문자와 문자열, Time: 100 ms, Memory: 14188 KB -BaekjoonHub

---
+++ 백준/Bronze/27866. 문자와 문자열/문자와 문자열.java
@@ -0,0 +1,13 @@
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) throws Exception{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        
+        String s = br.readLine();
+        int n = Integer.parseInt(br.readLine());
+        System.out.println(s.charAt(n - 1));
+        
+        br.close();
+    }
+}
