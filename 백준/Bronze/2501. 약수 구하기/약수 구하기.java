1a5d28aff60157650efb903e7c18cf1afedb6787
wonsyn <82020255+wonsyn@users.noreply.github.com>
2024-10-25 17:15:09 +0900

    [Bronze III] Title: 약수 구하기, Time: 108 ms, Memory: 14256 KB -BaekjoonHub

---
+++ 백준/Bronze/2501. 약수 구하기/약수 구하기.java
@@ -0,0 +1,26 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws Exception{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int N = Integer.parseInt(st.nextToken());
+        int M = Integer.parseInt(st.nextToken());
+
+        int cnt = 0, ans = 0;
+        for(int i = 1; i <= N; i++) {
+            if(N % i == 0) {
+                cnt++;
+                if(cnt == M) {
+                    ans = i;
+                    break;
+                }
+            }
+        }
+
+        System.out.println(ans);
+        br.close();
+    }
+}
