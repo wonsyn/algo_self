40ace21c68116cacb9c9dc5d04a32a5468d2cc3c
wonsyn <82020255+wonsyn@users.noreply.github.com>
2024-10-31 15:07:40 +0900

    [Bronze III] Title: 알고리즘 수업 - 알고리즘의 수행 시간 5, Time: 164 ms, Memory: 17736 KB -BaekjoonHub

---
+++ 백준/Bronze/24266. 알고리즘 수업 － 알고리즘의 수행 시간 5/알고리즘 수업 － 알고리즘의 수행 시간 5.java
@@ -0,0 +1,12 @@
+import java.math.BigInteger;
+import java.util.*;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        BigInteger n = new BigInteger(sc.next());
+        System.out.println(n.multiply(n).multiply(n));
+        System.out.println(3);
+    }
+}
