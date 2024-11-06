d3f418154b4db05feb7c64a1a01ac867c2573f47
wonsyn <82020255+wonsyn@users.noreply.github.com>
2024-11-06 16:58:23 +0900

    [Bronze I] Title: 약수, Time: 104 ms, Memory: 14308 KB -BaekjoonHub

---
+++ 백준/Bronze/1037. 약수/약수.java
@@ -0,0 +1,21 @@
+import java.io.*;
+import java.util.*;
+
+public class Main{
+    public static void main(String[] args)throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int N = Integer.parseInt(br.readLine());
+        int max = Integer.MIN_VALUE;
+        int min = Integer.MAX_VALUE;
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        for(int i = 0; i < N; i++) {
+            int num = Integer.parseInt(st.nextToken());
+            max = Math.max(max, num);
+            min = Math.min(min, num);
+        }
+
+        System.out.println(max * min);
+        br.close();
+    }
+}
