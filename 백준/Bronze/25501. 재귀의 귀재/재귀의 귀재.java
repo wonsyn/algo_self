d80b9a2e6790514f7603d514ac5abccae534e190
wonsyn <82020255+wonsyn@users.noreply.github.com>
2024-11-06 17:39:30 +0900

    [Bronze II] Title: 재귀의 귀재, Time: 204 ms, Memory: 19036 KB -BaekjoonHub

---
+++ 백준/Bronze/25501. 재귀의 귀재/재귀의 귀재.java
@@ -0,0 +1,29 @@
+import java.io.*;
+
+public class Main{
+    static int cnt;
+    public static int recursion(String s, int l, int r){
+        cnt++;
+        if(l >= r) return 1;
+        else if(s.charAt(l) != s.charAt(r)) return 0;
+        else return recursion(s, l+1, r-1);
+    }
+    public static int isPalindrome(String s){
+        return recursion(s, 0, s.length()-1);
+    }
+    public static void main(String[] args) throws Exception{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+
+        int T = Integer.parseInt(br.readLine());
+
+        for(int t = 0; t < T; t++) {
+            String s = br.readLine();
+            cnt = 0;
+            sb.append(isPalindrome(s)).append(" ").append(cnt).append("\n");
+        }
+
+        System.out.println(sb);
+        br.close();
+    }
+}
