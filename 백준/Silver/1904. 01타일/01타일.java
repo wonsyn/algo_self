baac26ca0b0c3c537655a1a0ebc993e770de8c59
wonsyn <82020255+wonsyn@users.noreply.github.com>
2024-11-14 16:26:31 +0900

    [Silver III] Title: 01타일, Time: 120 ms, Memory: 18096 KB -BaekjoonHub

---
+++ 백준/Silver/1904. 01타일/01타일.java
@@ -0,0 +1,21 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+public class Main{
+    public static void main(String[] args) throws Exception{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int N = Integer.parseInt(br.readLine());
+
+        int[] dp = new int[N + 1];
+
+        dp[1] = 1;
+        if(N != 1) dp[2] = 2;
+        for(int i = 3; i <= N; i++) {
+            dp[i] = (dp[i - 2] + dp[i - 1]) % 15746;
+        }
+
+        System.out.println(dp[N]);
+        br.close();
+    }
+}
