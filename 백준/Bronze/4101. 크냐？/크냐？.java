316935946d0b4067c56302318f02ea349b0c3ee2
wonsyn <82020255+wonsyn@users.noreply.github.com>
2024-12-02 22:06:38 +0900

    [Bronze V] Title: 크냐?, Time: 104 ms, Memory: 14240 KB -BaekjoonHub

---
+++ 백준/Bronze/4101. 크냐？/크냐？.java
@@ -0,0 +1,30 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main{
+    public static void main(String[] args) throws Exception{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+
+        while(true) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            int a = Integer.parseInt(st.nextToken());
+            int b = Integer.parseInt(st.nextToken());
+
+            if(a == 0 && b == 0) {
+                break;
+            }
+
+            if(a > b) {
+                sb.append("Yes");
+            } else {
+                sb.append("No");
+            }
+            sb.append("\n");
+        }
+
+        System.out.println(sb);
+        br.close();
+    }
+}
