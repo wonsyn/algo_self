8042e1bcac734da794051c729244c4b524b4ac9a
wonsyn <82020255+wonsyn@users.noreply.github.com>
2024-12-18 12:59:20 +0900

    [Bronze III] Title: 별 찍기 - 4, Time: 104 ms, Memory: 14168 KB -BaekjoonHub

---
+++ 백준/Bronze/2441. 별 찍기 － 4/별 찍기 － 4.java
@@ -0,0 +1,19 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+public class Main{
+    public static void main(String[] args) throws Exception{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int N = Integer.parseInt(br.readLine());
+        StringBuilder sb = new StringBuilder();
+        for(int i = 0; i < N; i++) {
+            sb.append(" ".repeat(i))
+                    .append("*".repeat(N - i))
+                    .append("\n");
+        }
+
+        System.out.println(sb);
+        br.close();
+    }
+}
