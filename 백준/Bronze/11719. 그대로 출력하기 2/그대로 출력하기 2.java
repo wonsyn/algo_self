4a1d3940d7dc321c131778d66e520f7f87b8802f
wonsyn <82020255+wonsyn@users.noreply.github.com>
2024-12-20 13:23:32 +0900

    [Bronze III] Title: 그대로 출력하기 2, Time: 112 ms, Memory: 14304 KB -BaekjoonHub

---
+++ 백준/Bronze/11719. 그대로 출력하기 2/그대로 출력하기 2.java
@@ -0,0 +1,21 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+public class Main{
+    public static void main(String[] args) throws Exception{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        StringBuilder sb = new StringBuilder();
+
+        while(true) {
+            String str = br.readLine();
+            if(str == null) {
+                break;
+            }
+            sb.append(str).append("\n");
+        }
+
+        System.out.println(sb);
+        br.close();
+    }
+}
