72c886f6af586b61213f5217d872447751adccd5
wonsyn <82020255+wonsyn@users.noreply.github.com>
2025-01-13 16:24:58 +0900

    [Bronze IV] Title: !밀비 급일, Time: 100 ms, Memory: 14048 KB -BaekjoonHub

---
+++ 백준/Bronze/11365. ！밀비 급일/！밀비 급일.java
@@ -0,0 +1,22 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+public class Main{
+    public static void main(String[] args) throws Exception{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+
+        while(true) {
+            String s = br.readLine();
+
+            if(s.equals("END")) {
+                break;
+            }
+
+            sb.append(new StringBuffer(s).reverse()).append("\n");
+        }
+
+        System.out.println(sb);
+        br.close();
+    }
+}
