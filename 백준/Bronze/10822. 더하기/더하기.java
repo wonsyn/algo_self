fbbeb87eeb69c9e9ccb1198d1202980463d3b8ea
wonsyn <82020255+wonsyn@users.noreply.github.com>
2025-02-09 02:32:55 +0900

    [Bronze II] Title: 더하기, Time: 104 ms, Memory: 14204 KB -BaekjoonHub

---
+++ 백준/Bronze/10822. 더하기/더하기.java
@@ -0,0 +1,19 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main{
+    public static void main(String[] args) throws Exception{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int sum = 0;
+        StringTokenizer st = new StringTokenizer(br.readLine(), ",");
+
+        while(st.hasMoreTokens()) {
+            sum += Integer.parseInt(st.nextToken());
+        }
+
+        System.out.println(sum);
+        br.close();
+    }
+}
