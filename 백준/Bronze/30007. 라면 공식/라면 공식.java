adc5859a936c2d708c5d1be0e0f6fffccd0001c2
wonsyn <82020255+wonsyn@users.noreply.github.com>
2025-02-16 12:12:39 +0900

    [Bronze V] Title: 라면 공식, Time: 108 ms, Memory: 14140 KB -BaekjoonHub

---
+++ 백준/Bronze/30007. 라면 공식/라면 공식.java
@@ -0,0 +1,25 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main{
+    public static void main(String[] args) throws Exception{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int N = Integer.parseInt(br.readLine());
+        StringBuilder sb = new StringBuilder();
+
+        for(int i = 0; i < N; i++) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+
+            int A = Integer.parseInt(st.nextToken());
+            int B = Integer.parseInt(st.nextToken());
+            int X = Integer.parseInt(st.nextToken());
+
+            sb.append((X - 1) * A + B).append("\n");
+        }
+
+        System.out.println(sb);
+        br.close();
+    }
+}
