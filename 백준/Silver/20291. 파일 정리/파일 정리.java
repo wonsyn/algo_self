7f701578b483448d5c0a8b9fd27465598b76d3c1
wonsyn <82020255+wonsyn@users.noreply.github.com>
2025-03-05 16:12:47 +0900

    [Silver III] Title: 파일 정리, Time: 1048 ms, Memory: 57016 KB -BaekjoonHub

---
+++ 백준/Silver/20291. 파일 정리/파일 정리.java
@@ -0,0 +1,41 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.*;
+
+public class Main{
+    public static void main(String[] args) throws Exception{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+
+        Map<String, Integer> map = new HashMap<>();
+
+        for(int i = 0; i < N; i++) {
+            StringTokenizer st = new StringTokenizer(br.readLine(), ".");
+            st.nextToken();
+            String ex = st.nextToken();
+
+            map.put(ex, map.getOrDefault(ex, 0) + 1);
+        }
+        Set<String> keys = map.keySet();
+        ArrayList<Ex> list = new ArrayList<>();
+        for(String k : keys) {
+            list.add(new Ex(k, map.get(k)));
+        }
+        list.sort((o1, o2) -> o1.ex.compareTo(o2.ex));
+        for(Ex ex : list) {
+            System.out.println(ex);
+        }
+        br.close();
+    }
+    static class Ex {
+        String ex;
+        int cnt;
+        Ex(String ex, int cnt) {
+            this.ex = ex; this.cnt = cnt;
+        }
+
+        public String toString() {
+            return ex + " " + cnt;
+        }
+    }
+}
