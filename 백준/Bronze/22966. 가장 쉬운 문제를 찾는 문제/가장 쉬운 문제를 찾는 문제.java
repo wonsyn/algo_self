4856f623b4ec1c9c36527f00a3ec2b8154178330
wonsyn <82020255+wonsyn@users.noreply.github.com>
2025-03-06 18:37:20 +0900

    [Bronze II] Title: 가장 쉬운 문제를 찾는 문제, Time: 100 ms, Memory: 14180 KB -BaekjoonHub

---
+++ 백준/Bronze/22966. 가장 쉬운 문제를 찾는 문제/가장 쉬운 문제를 찾는 문제.java
@@ -0,0 +1,26 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main{
+    public static void main(String[] args) throws Exception{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+
+        int min = Integer.MAX_VALUE;
+        String minName = "";
+
+        for(int i = 0; i < N; i++) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            String name = st.nextToken();
+            int n = Integer.parseInt(st.nextToken());
+            if(n < min) {
+                minName = name;
+                min = n;
+            }
+        }
+
+        System.out.println(minName);
+        br.close();
+    }
+}
