51acfb93cf84b140acdb69873b6040f11c4189bc
wonsyn <82020255+wonsyn@users.noreply.github.com>
2025-03-27 21:51:36 +0900

    [Bronze IV] Title: 선린인터넷고등학교 교가, Time: 116 ms, Memory: 15480 KB -BaekjoonHub

---
+++ 백준/Bronze/21964. 선린인터넷고등학교 교가/선린인터넷고등학교 교가.java
@@ -0,0 +1,13 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+public class Main{
+    public static void main(String[] args) throws Exception{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+        String s = br.readLine();
+        System.out.println(s.substring(N - 5));
+
+        br.close();
+    }
+}
