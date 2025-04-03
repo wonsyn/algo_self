860cf10f6503f09c717e60c0c038126d0c1460f3
wonsyn <82020255+wonsyn@users.noreply.github.com>
2025-04-03 21:34:16 +0900

    [Silver V] Title: 스네이크버드, Time: 112 ms, Memory: 14612 KB -BaekjoonHub

---
+++ 백준/Silver/16435. 스네이크버드/스네이크버드.java
@@ -0,0 +1,31 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class Main{
+    public static void main(String[] args) throws Exception{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int N = Integer.parseInt(st.nextToken());
+        int l = Integer.parseInt(st.nextToken());
+
+        int[] arr = new int[N];
+
+        st = new StringTokenizer(br.readLine());
+        for(int i = 0; i < N; i++) {
+            arr[i] = Integer.parseInt(st.nextToken());
+        }
+        Arrays.sort(arr);
+        for(int i = 0; i < N; i++) {
+            if(arr[i] <= l) {
+                l++;
+            } else {
+                break;
+            }
+        }
+        System.out.println(l);
+        br.close();
+    }
+}
