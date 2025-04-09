3688758498ffebc809099e74f34f5ac448e74773
wonsyn <82020255+wonsyn@users.noreply.github.com>
2025-04-09 20:23:52 +0900

    [Bronze IV] Title: 평균 점수, Time: 108 ms, Memory: 14148 KB -BaekjoonHub

---
+++ 백준/Bronze/10039. 평균 점수/평균 점수.java
@@ -0,0 +1,21 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+public class Main{
+    public static void main(String[] args) throws Exception{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int sum = 0;
+        for(int i = 0; i < 5; i++) {
+            int n = Integer.parseInt(br.readLine());
+            if(n < 40) {
+                sum += 40;
+            } else {
+                sum += n;
+            }
+        }
+
+        System.out.println(sum / 5);
+        br.close();
+    }
+}
