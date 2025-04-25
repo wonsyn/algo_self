2fd330dab951620fcaa9f15827e898896d381dd0
wonsyn <82020255+wonsyn@users.noreply.github.com>
2025-04-25 16:36:40 +0900

    [Gold IV] Title: 이모티콘, Time: 124 ms, Memory: 20300 KB -BaekjoonHub

---
+++ 백준/Gold/14226. 이모티콘/이모티콘.java
@@ -0,0 +1,55 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    static int S, res = Integer.MAX_VALUE;
+    static boolean[][] visited;
+
+    public static void main(String[] args) throws Exception{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        S = Integer.parseInt(br.readLine());
+        visited = new boolean[2001][2001];
+
+        bfs();
+        System.out.println(res);
+        br.close();
+    }
+
+    static void bfs() {
+        Queue<Node> q = new ArrayDeque<>();
+        q.offer(new Node(0, 1, 0));
+
+        while(!q.isEmpty()) {
+            Node cur = q.poll();
+            if(cur.now == S) {
+                res = Math.min(res, cur.time);
+                break;
+            }
+
+            //클립보드 저장
+            if(!visited[cur.now][cur.now]) {
+                q.offer(new Node(cur.now, cur.now, cur.time + 1));
+            }
+            //클립보드 붙여넣기
+            if(cur.now + cur.clip <= 2000 && !visited[cur.clip][cur.now + cur.clip] && cur.clip != 0) {
+                visited[cur.clip][cur.now + cur.clip] = true;
+                q.offer(new Node(cur.clip, cur.now + cur.clip, cur.time + 1));
+            }
+            //이모티콘 1개 삭제
+            if(!visited[cur.clip][cur.now - 1] && cur.now != 1) {
+                visited[cur.clip][cur.now - 1] = true;
+                q.offer(new Node(cur.clip, cur.now - 1, cur.time + 1));
+            }
+        }
+    }
+
+    static class Node {
+        int clip;
+        int now;
+        int time;
+        Node(int clip, int now, int time) {
+            this.clip = clip; this.now = now; this.time = time;
+        }
+    }
+}
