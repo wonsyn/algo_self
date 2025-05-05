ed7411c0cd4cf4f9e9c08d6207eac28a63709a26
wonsyn <82020255+wonsyn@users.noreply.github.com>
2025-05-05 12:40:17 +0900

    [Silver II] Title: 최소 힙, Time: 1076 ms, Memory: 30584 KB -BaekjoonHub

---
+++ 백준/Silver/1927. 최소 힙/최소 힙.java
@@ -0,0 +1,25 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Comparator;
+import java.util.PriorityQueue;
+
+public class Main {
+
+	public static void main(String[] args) throws IOException {
+    	BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+
+        PriorityQueue<Integer> pq = new PriorityQueue<>(Comparator.comparingInt(x -> x));
+
+        for(int i = 0 ; i < n; i++){
+            int val = Integer.parseInt(br.readLine());
+            if(val == 0){
+                if(pq.isEmpty()) System.out.println("0");
+                else System.out.println(pq.poll());
+            } else{
+                pq.add(val);
+            }
+        }
+    }
+}
