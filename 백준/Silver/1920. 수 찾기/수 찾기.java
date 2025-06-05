6cbd23abf4c49e2b4df806405a656e758d13f40d
wonsyn <82020255+wonsyn@users.noreply.github.com>
2025-06-05 15:35:14 +0900

    [Silver IV] Title: 수 찾기, Time: 564 ms, Memory: 45428 KB -BaekjoonHub

--- 백준/Silver/1920. 수 찾기/수 찾기.java
+++ 백준/Silver/1920. 수 찾기/수 찾기.java
@@ -1,7 +1,6 @@
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
-import java.util.HashSet;
-import java.util.Set;
+import java.util.Arrays;
 import java.util.StringTokenizer;
 
 public class Main{
@@ -9,17 +8,35 @@ public class Main{
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringBuilder sb = new StringBuilder();
 
-        Set<Integer> set = new HashSet<>();
         int N = Integer.parseInt(br.readLine());
         StringTokenizer st = new StringTokenizer(br.readLine());
-        while(st.hasMoreTokens()) {
-            set.add(Integer.parseInt(st.nextToken()));
+        int[] arrN = new int[N];
+        for(int i = 0; i < N; i++) {
+            arrN[i] = Integer.parseInt(st.nextToken());
         }
 
         int M = Integer.parseInt(br.readLine());
         st = new StringTokenizer(br.readLine());
-        while(st.hasMoreTokens()) {
-            sb.append(set.contains(Integer.parseInt(st.nextToken())) ? 1 : 0).append("\n");
+        int[] arrM = new int[M];
+        for(int i = 0; i < M; i++) {
+            arrM[i] = Integer.parseInt(st.nextToken());
+        }
+
+        Arrays.sort(arrN);
+        for(int m : arrM) {
+            int start = 0, end = N - 1;
+            boolean flag = false;
+            while(start < end) {
+                int mid = (start + end) / 2;
+
+                if (m > arrN[mid]) {
+                    start = mid + 1;
+                } else {
+                    end = mid;
+                }
+            }
+
+            sb.append(arrN[end] == m ? 1 : 0).append("\n");
         }
         System.out.print(sb);
         br.close();
