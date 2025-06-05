1291595d9b1a10ca2f6df9dbf1097098d340bda2
wonsyn <82020255+wonsyn@users.noreply.github.com>
2025-06-05 16:54:19 +0900

    [Silver III] Title: 프린터 큐, Time: 116 ms, Memory: 14860 KB -BaekjoonHub

--- 백준/Silver/1966. 프린터 큐/프린터 큐.java
+++ 백준/Silver/1966. 프린터 큐/프린터 큐.java
@@ -1,60 +1,52 @@
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.util.ArrayDeque;
-import java.util.Comparator;
-import java.util.PriorityQueue;
-import java.util.Queue;
+import java.util.ArrayList;
 import java.util.StringTokenizer;
 
-public class Main {
+public class Main{
+    public static void main(String[] args) throws Exception{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
 
-	public static void main(String[] args) throws Exception {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringTokenizer st;
-		StringBuilder sb = new StringBuilder();
-		
-		int T = Integer.parseInt(br.readLine());
-		
-		for(int tc = 0; tc < T; tc++) {
-			st = new StringTokenizer(br.readLine());
-			int N = Integer.parseInt(st.nextToken());
-			int M = Integer.parseInt(st.nextToken());
-			int[] priority = new int[11];
-			
-			Queue<int[]> q = new ArrayDeque<>();
-			
-			st = new StringTokenizer(br.readLine());
-			
-			int count = 0;
-			
-			for(int i = 0; i < N; i++) {
-				int num = Integer.parseInt(st.nextToken());
-				q.offer(new int[] {i, num});
-				priority[num]++;
-			}
-			
-			while(!q.isEmpty()) {
-				int[] cur = q.poll();
-				boolean flag = true;
-				
-				for(int i = cur[1] + 1; i < 11; i++) {
-					if(priority[i] != 0) {
-						flag = false;
-						q.offer(cur);
-						break;
-					}
-				}
-				
-				if(!flag) continue;
-				
-				count++;
-				priority[cur[1]]--;
-				if(cur[0] == M) break;
-			}
-			
-			sb.append(count).append("\n");
-		}
-		
-		System.out.println(sb.toString());
-	}
+        int T = Integer.parseInt(br.readLine());
+
+        for(int tc = 0; tc < T; tc++) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            int N = Integer.parseInt(st.nextToken());
+            int M = Integer.parseInt(st.nextToken());
+
+            ArrayList<Integer> q = new ArrayList<>();
+            st = new StringTokenizer(br.readLine());
+            for(int i = 0; i < N; i++) {
+                q.add(Integer.parseInt(st.nextToken()));
+            }
+
+            int cnt = 0;
+            while(M >= 0 && N > 0) {
+                boolean flag = false;
+                int n = q.get(0);
+                for(int i = 1; i < N; i++) {
+                    if(q.get(i) > n) {
+                        q.add(q.remove(0));
+                        if(M == 0) M += N - 1;
+                        else M--;
+                        flag = true;
+                        break;
+                    }
+                }
+
+                if(!flag) {
+                    q.remove(0);
+                    cnt++;
+                    N--;
+                    M--;
+                }
+            }
+            sb.append(cnt).append("\n");
+        }
+
+        System.out.print(sb);
+        br.close();
+    }
 }
