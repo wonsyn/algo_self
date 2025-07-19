d3d820a08a0930702d21d33a9ddf3f1921ec5d01
wonsyn <82020255+wonsyn@users.noreply.github.com>
2025-07-19 15:48:25 +0900

    [Silver IV] Title: 수 이어 쓰기 1, Time: 104 ms, Memory: 14208 KB -BaekjoonHub

--- 백준/Silver/1748. 수 이어 쓰기 1/수 이어 쓰기 1.java
+++ 백준/Silver/1748. 수 이어 쓰기 1/수 이어 쓰기 1.java
@@ -5,22 +5,25 @@ public class Main {
     public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
-        int N = Integer.parseInt(br.readLine());
+        String s = br.readLine();
+        br.close();
+
+        int N = Integer.parseInt(s);
 
         int cnt = 0;
-        for(int i = 1; i <= N; i++) {
-            if(i < 10) cnt += 1;
-            else if(i < 100) cnt += 2;
-            else if(i < 1000) cnt += 3;
-            else if(i < 10000) cnt += 4;
-            else if(i < 100000) cnt += 5;
-            else if(i < 1000000) cnt += 6;
-            else if(i < 10000000) cnt += 7;
-            else if(i < 100000000) cnt += 8;
-            else cnt += 9;
+        long low = 1, up = 9;
+        int len = 1;
+
+        while(low <= N) {
+            if(up > N) up = N;
+
+            cnt += len * (up - low + 1);
+
+            low *= 10;
+            up = 10 * up + 9;
+            len++;
         }
 
         System.out.println(cnt);
-        br.close();
     }
 }
